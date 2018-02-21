cba0c719c030aac3784f65a7e690bdd779eec7de
Sumika H <sumikah@hotmail.com>
2018-02-21 21:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,19 +5,7 @@ function happyHolidays(){
 happyHolidays()
 
 function happyHolidaysTo(name){
-  return Happy holidays, ${name}!
+  return `Happy holidays, ${name}!`
 }
 
 happyHolidaysTo('you')
-
-function happyHolidaysTo(holiday, name){
-  return `Happy ${holiday}, ${name}!`
-}
-happyHolidaysTo('Independence Day', 'you')
-
-
-function holidayCountdown(days, holiday){
-  return `It\'s ${days} days until ${holiday}!`
-
-}
-  holidayCountdown (20, 'Mother\'s Day')
